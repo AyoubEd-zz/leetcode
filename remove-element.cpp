ce5b9f17f31d33650c11a46b7649a387da779c8c
AyoubEd <ayoube.eddakhly@gmail.com>
2019-02-12 06:30:09 +0100

    remove element

---
+++ remove-element.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/remove-element/submissions/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        if(n==0 || (n==1 && nums[0]==val)) return 0;
+        if(n==1) return 1;
+        int sp = 0, wp = 0;
+        while(sp<n){
+            if(nums[sp]!=val){
+                nums[wp++]=nums[sp];
+            }
+            sp++;
+        }
+        return wp;
+    }
+};
