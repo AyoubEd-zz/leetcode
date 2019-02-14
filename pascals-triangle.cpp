0f1a43a5f92799b085a132262e20cea14e824fd1
AyoubEd <ayoube.eddakhly@gmail.com>
2019-02-14 07:08:09 +0100

    pascals triangle

--- pascals-triangle.cpp
+++ pascals-triangle.cpp
@@ -5,13 +5,18 @@ public:
         vector<vector<int>> v;
         if(numRows==0) return v;
         v.push_back({1});
-        for(int i=0 ; i<numRows-1 ; i++){
-            vector<int> vs={1};
+        if(numRows==1) return v;
+        v.push_back({1, 1});
+        for(int i=1 ; i<numRows-1 ; i++){
             vector<int> vp = v[i];
-            for(int j=1; j<vp.size() ; j++){
-                vs.push_back(vp[j-1]+vp[j]);
+            int n = vp.size();
+            vector<int> vs(n+1);
+            vs[0]=1;
+            for(int j=1; j<=(int)(n+1)/2; j++){
+                vs[j]=vp[j-1]+vp[j];
+                vs[n-j]=vs[j];
             }
-            vs.push_back(1);
+            vs[n]=1;
             v.push_back(vs);
         }
         return v;
