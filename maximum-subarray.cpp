7443161b713a821859220b39821f422ec367674b
AyoubEd <ayoube.eddakhly@gmail.com>
2019-02-15 07:21:06 +0100

    maximum subarray | Kadane's Algorithm

--- maximum-subarray.cpp
+++ maximum-subarray.cpp
@@ -7,8 +7,8 @@ public:
         int sum = 0;
         int max_sum = INT_MIN;
         for(int i=0;i<n;i++){
-            if(sum<0) sum = nums[i];
-            else sum+=nums[i];
+            if(sum<0) sum = 0;
+            sum+=nums[i];
             if(sum>max_sum) max_sum = sum;
         }
         return max_sum;
