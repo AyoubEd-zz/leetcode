4ee3cb4be4cd10d49d4058e94c89dd7526605aa3
AyoubEd <ayoube.eddakhly@gmail.com>
2019-02-23 08:14:00 +0100

    Two pointer and swap like QuickSort

--- sort-array-by-parity.cpp
+++ sort-array-by-parity.cpp
@@ -1,20 +1,22 @@
 /*https://leetcode.com/problems/sort-array-by-parity
  * Solution: Two Pointer and swapping like QuickSort
+*
 */
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& A) {
-        int i = 0, j = A.size()-1, a, b;
-        while(i<j){
-            a = (A[i]%2!=0);
-            b = (A[j]%2==0);
-            if(a==0 && b==0){i++;j--;}
-            else if(a==1 && b==1){
-                swap(A[i], A[j]);
-                i++;j--;
-            }else if(a==0 && b==1) i++;
-            else j--;
+        int i = 0, j = A.size() - 1;
+        while (i < j) {
+            if (A[i] % 2 > A[j] % 2) {
+                int tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+
+            if (A[i] % 2 == 0) i++;
+            if (A[j] % 2 == 1) j--;
         }
+
         return A;
     }
 };
