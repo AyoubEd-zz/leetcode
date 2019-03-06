7ab084c1d9b252dcfa350f80a4e74f1c37a0f009
AyoubEd <ayoube.eddakhly@gmail.com>
2019-03-06 06:50:07 +0100

    -

---
+++ can-place-flowers.cpp
@@ -0,0 +1,21 @@
+/* https://leetcode.com/problems/can-place-flowers/
+ * ps: insert zeros at the end and the begnning
+*/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& bed, int n) {
+        int count = 0;
+        bed.insert(bed.begin(), 0);
+        bed.push_back(0);
+        for(int i=1 ; i<bed.size()-1 ; i++){
+            if(bed[i-1]+bed[i]+bed[i+1]==0){
+                count++;
+                bed[i]=1;
+            }
+            if(count>=n)return 1;
+        }
+        if(count>=n)return 1;
+        else return 0;
+    }
+}
