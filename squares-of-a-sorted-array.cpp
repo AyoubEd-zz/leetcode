24ac4c28274b6a81110a450a465db067bb778e39
AyoubEd <ayoube.eddakhly@gmail.com>
2019-03-14 08:37:36 +0100

    two pointers

--- squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array.cpp
@@ -1,21 +1,25 @@
 /*https://leetcode.com/problems/squares-of-a-sorted-array
-*Solution: Two Pointers and fill from end
-*/
+ *Solution: Two Pointers and fill from end
+ */
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& A) {
-        int i = 0, j = A.size()-1 , k = A.size()-1;
+        int i = 0, j = A.size()-1, k = A.size()-1, pi, pj;
         vector<int> v(A.size());
-        while(i<=j){
-            if((-A[i])>=A[j]){
-                v[k] = A[i]*A[i];
-                i++;
-            }else{
-                v[k] = A[j]*A[j];
+        while(i<j){
+            pi = A[i] * A[i];
+            pj = A[j] * A[j];
+            if (pi < pj) {
+                v[k] = pj;
                 j--;
+            } else {
+                v[k] = pi;
+                i++;
             }
             k--;
         }
+        v[k] = A[i]*A[i];
         return v;
     }
 };
