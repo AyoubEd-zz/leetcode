b7f92ca56264ca1f9709500b8cf8b0c36f02e8ae
AyoubEd <ayoube.eddakhly@gmail.com>
2019-03-15 06:40:23 +0100

    two pointers

--- sort-array-by-parity.cpp
+++ sort-array-by-parity.cpp
@@ -8,9 +8,7 @@ public:
         int i = 0, j = A.size() - 1;
         while (i < j) {
             if (A[i] % 2 > A[j] % 2) {
-                int tmp = A[i];
-                A[i] = A[j];
-                A[j] = tmp;
+                    swap(A[i], A[j]);
             }
 
             if (A[i] % 2 == 0) i++;
