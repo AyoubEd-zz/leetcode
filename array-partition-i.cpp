ebae59c1a98fbc6ce7b25b437574376ae27718e5
AyoubEd <ayoube.eddakhly@gmail.com>
2019-03-15 08:47:47 +0100

    vector indices to sort

--- array-partition-i.cpp
+++ array-partition-i.cpp
@@ -1,15 +1,21 @@
 /*https://leetcode.com/problems/array-partition-i/
- * Solution: Sort and pair largest with second largest element and so on.
-*/
+ * Solution: Use vector indices to naturally sort. 
+ */
+
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int i=0, sum=0;
-        while(i<nums.size()){
-            sum+=nums[i];
-            i+=2;
+        vector<int> v(20001, 0);
+        for(int i:nums) v[i+10000]++;
+        int res=0, flag=1, i=0;
+        while(i<20001){
+            if(v[i]>0){
+                res = flag? res+i-10000 : res;
+                flag = flag ^ 1;
+                v[i]--;
+            }
+            if(v[i]==0) i++;
         }
-        return sum;
+        return res;
     }
 };
