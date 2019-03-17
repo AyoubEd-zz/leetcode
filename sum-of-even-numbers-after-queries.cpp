6d1689b5f239a516a3db3cd712143951327f1fea
AyoubEd <ayoube.eddakhly@gmail.com>
2019-03-17 06:12:01 +0100

    precalculate and update

--- sum-of-even-numbers-after-queries.cpp
+++ sum-of-even-numbers-after-queries.cpp
@@ -1,28 +1,22 @@
 /*https://leetcode.com/problems/sum-of-even-numbers-after-queries
- * Solution: Get the sum at the beginning and maintain it
+ * Solution: Get the sum at the beginning and update it
 */
 
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& queries) {
+        int sum=0, index, val;
         vector<int> v(queries.size());
-        int sum = 0;
-        for(int i:A){if(i%2==0)sum+=i;}
-        for(int i=0; i<queries.size() ; i++){
-            int val = queries[i][0], idx = queries[i][1];
-            if(val%2==0 && A[idx]%2==0){
-                sum+=val;
-                A[idx]+=val;
-            }else if(val%2!=0 && A[idx]%2!=0){
-                A[idx]+=val;
-                sum+=A[idx];
-            }else if(val%2!=0 && A[idx]%2==0){
-                sum-=A[idx];
-                A[idx]+=val;
-            }else {
-                A[idx]+=val;
-            }
-            v[i]=sum;
+        for(auto a:A){
+            if(a%2==0) sum+=a;
+        }
+        for(int i=0 ; i<queries.size()  ; i++){
+            index = queries[i][1];
+            val = queries[i][0];
+            if(A[index]%2==0) sum-=A[index];
+            A[index]+=val;
+            if(A[index]%2==0) sum+=A[index];
+            v[i] = sum;
         }
         return v;
     }
