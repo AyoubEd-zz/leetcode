5141a7b0489ac06e3a041d0a7a6ffc5cc2b1a0d8
AyoubED <ayoube.eddakhly@gmail.com>
2021-08-28 17:48:05 +0200

    Time: 37 ms (11.60%), Space: 22.5 MB (47.10%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        if(A.size()<3) return 0;
+        int i = 1, a = 0, b = 0;
+        while(i<A.size() && A[i]>A[i-1]){
+            if(a==0) a++;
+            i++;
+        }
+        i--;
+        while(i<A.size()-1 && A[i]>A[i+1]){
+            if(b==0) b++;
+            i++;
+        }
+        return (i==A.size()-1)?a*b:0;
+    }
+};
